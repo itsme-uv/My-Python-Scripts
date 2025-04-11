2d1d49ff44db45c2f395490ee1138abf51bebd15
yuvateja Guddanti <yuvatejaguddanti@yuvatejas-MacBook-Air.local>
2025-04-11 08:35:13 +0530

    commiting Greaterthan5.py

---
+++ Greaterthan5.py
@@ -0,0 +1,5 @@
+if ( 5 > 2 ):
+    print("Five is greater than two!")
+# Output: Five is greater than two!
+# The if statement is used to test a condition.
+# If the condition is true, the code block inside the if statement will be executed.
