71006bd9e2aa0897bb6771e4c9bc8e44d7e1d17c
ITSmeUV <yuvatejaguddanti@yuvatejas-MacBook-Air.local>
2025-04-11 08:49:53 +0530

    commiting variable.py

---
+++ variable.py
@@ -0,0 +1,16 @@
+x = 5
+y = "yuva" 
+# # The variable x is assigned the value 5, and the variable y is assigned the string "yuva".
+# # The variable x is an integer, and the variable y is a string.
+# # The type of the variable can be determined using the type() function.
+print(x)
+print(y)
+# # Output: 5
+# # Output: yuva
+# # The print() function is used to display the value of the variable.
+# # The output will be:
+# # 5
+# # yuva
+# # The type of the variable can be determined using the type() function.
+# # The type of the variable can be determined using the type() function.
+# # The type of the variable can be determined using the type() function.
